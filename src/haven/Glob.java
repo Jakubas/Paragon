f2d24b2390b228ca22d1562cfacf8ada5929582d
romovs <romovs@gmail.com>
2015-08-31 17:12:55 +0300

    Make things less bright in daylight mode

--- src/haven/Glob.java
+++ src/haven/Glob.java
@@ -48,7 +48,7 @@ public class Glob {
     public Color lightamb = null, lightdif = null, lightspc = null;
     public Color olightamb = null, olightdif = null, olightspc = null;
     public Color tlightamb = null, tlightdif = null, tlightspc = null;
-    public static Color dlightamb = new Color(255, 255, 255);
+    public static Color dlightamb = new Color(200, 200, 200);
     public static Color dlightspc = new Color(255, 255, 255);
     public double lightang = 0.0, lightelev = 0.0;
     public double olightang = 0.0, olightelev = 0.0;
