87fa57d643f45b4af4127cd13a099bf6388a95f5
romovs <romovs@gmail.com>
2015-09-09 01:08:43 +0300

    Updated boulders list

--- src/haven/Config.java
+++ src/haven/Config.java
@@ -88,7 +88,7 @@ public class Config {
 
     public final static String[] boulders = new String[]{"basalt", "limonite", "schist", "dolomite", "magnetite", "gneiss",
             "granite", "malachite", "hematite", "porphyry", "ilmenite", "quartz", "cassiterite", "limestone", "sandstone",
-            "chalcopyrite", "cinnabar", "feldspar", "marble", "ras", "flint"};
+            "chalcopyrite", "cinnabar", "feldspar", "marble", "ras", "flint", "hornsilver"};
     public static String[] boulderssel = Utils.getprefsa("boulderssel", null);
 
     public final static String[] bushes = new String[]{"arrowwood", "crampbark", "sandthorn", "blackberrybush", "dogrose",
