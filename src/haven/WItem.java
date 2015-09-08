198b62c417c0864f887e4a6781f65e01935136cd
romovs <romovs@gmail.com>
2015-09-08 07:29:59 +0300

    Chnaged mass transfer key combination to use Alt since Shift was conflicting with spliting and mass planting

--- src/haven/WItem.java
+++ src/haven/WItem.java
@@ -244,7 +244,7 @@ public class WItem extends Widget implements DTarget {
                 item.wdgmsg("take", c);
             return (true);
         } else if (btn == 3) {
-            if (ui.modshift)
+            if (ui.modmeta)
                 wdgmsg("transfer-identical", item.resource().name);
             else
                 item.wdgmsg("iact", c, ui.modflags());
