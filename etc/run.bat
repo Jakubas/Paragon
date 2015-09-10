441b500c2fad5f043c6d53d46be327e0ceff80e7
romovs <romovs@gmail.com>
2015-09-10 08:26:33 +0300

    Updated run.bat to close the console window

--- etc/run.bat
+++ etc/run.bat
@@ -1,1 +1,1 @@
-java -Xms512m -Xmx1024m -jar hafen.jar -U http://game.havenandhearth.com/hres/ game.havenandhearth.com
+start javaw -Xms512m -Xmx1024m -jar hafen.jar -U http://game.havenandhearth.com/hres/ game.havenandhearth.com
