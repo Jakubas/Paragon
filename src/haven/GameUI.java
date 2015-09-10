baa352589fa7792f1958d1229a6788654e3dd25e
romovs <romovs@gmail.com>
2015-09-10 04:38:48 +0300

    Antialize progress percentage

--- src/haven/GameUI.java
+++ src/haven/GameUI.java
@@ -36,7 +36,7 @@ import static haven.Inventory.invsq;
 
 public class GameUI extends ConsoleHost implements Console.Directory {
     public static final Text.Foundry errfoundry = new Text.Foundry(Text.dfont, 14, new Color(192, 0, 0));
-    public static final Text.Foundry progressf = new Text.Foundry(Text.sansb, 12);
+    public static final Text.Foundry progressf = new Text.Foundry(Text.sansb, 12).aa(true);
     private static final int blpw = 142, brpw = 142;
     public final String chrid;
     public final long plid;
