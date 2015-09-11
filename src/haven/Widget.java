Jakubas/Paragon

commit 1e0e69167fca9cc81925c69afb27aaf8a1b8c4b3

Preserve window positions

src/haven/Widget.java
@@ -228,7 +228,16 @@ public class Widget {
     }
 
     public <T extends Widget> T add(T child, Coord c) {
-        child.c = c;
+        if (child instanceof Window) {
+            try {
+                Window wnd = (Window)child;
+                if (Window.persistentwnds.contains(wnd.cap.text))
+                    child.c = Utils.getprefc(wnd.cap.text + "_c", c);
+            } catch (Exception e) {
+            }
+        } else {
+            child.c = c;
+        }
         return (add(child));
     }
 

src/haven/Window.java
@@ -29,6 +29,9 @@ package haven;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import static haven.PUtils.*;
 
@@ -87,6 +90,9 @@ public class Window extends Widget implements DTarget {
     public int cmw;
     private UI.Grab dm = null;
     private Coord doff;
+    public static final Set<String> persistentwnds = new HashSet<String>(
+            Arrays.asList("Inventory", "Equipment", "Crafting", "Character Sheet", "Kith & Kin", "Crate", "Cupboard",
+                    "Barrel"));
 
     @RName("wnd")
     public static class $_ implements Factory {
@@ -275,6 +281,8 @@ public class Window extends Widget implements DTarget {
         if (dm != null) {
             dm.remove();
             dm = null;
+            if (persistentwnds.contains(cap.text))
+                Utils.setprefc(cap.text + "_c", this.c);
         } else {
             super.mouseup(c, button);
         }

commit fd328c4953550f03da1b246d2519fd02a4068dfb

Fixed regression introduced in 1e0e6916

src/haven/Widget.java
@@ -235,9 +235,8 @@ public class Widget {
                     child.c = Utils.getprefc(wnd.cap.text + "_c", c);
             } catch (Exception e) {
             }
-        } else {
-            child.c = c;
         }
+        child.c = c;
         return (add(child));
     }
 