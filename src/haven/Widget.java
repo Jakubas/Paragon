def28fd83f8ff9a001263bc679f364c36411fcd2
romovs <romovs@gmail.com>
2015-09-21 07:07:35 +0300

    Added method for getting Widget's GameUI

--- src/haven/Widget.java
+++ src/haven/Widget.java
@@ -1079,4 +1079,14 @@ public class Widget {
 
         public abstract void ntick(double a);
     }
+
+    public GameUI gameui() {
+        Widget parent = this.parent;
+        while (parent != null) {
+            if (parent instanceof GameUI)
+                return (GameUI) parent;
+            parent = parent.parent;
+        }
+        return null;
+    }
 }
