05c9544a0fed426a4778942f89ac7cf7d0789823
romovs <romovs@gmail.com>
2015-09-22 05:29:30 +0300

    Fixed crash when right clicking music instruments

--- src/haven/Text.java
+++ src/haven/Text.java
@@ -35,6 +35,7 @@ public class Text {
     public static final Font sans = new Font("Sans", Font.PLAIN, 10);
     public static final Font sansb = new Font("Sans", Font.BOLD, 10);
     public static final Font mono = new Font("Monospaced", Font.PLAIN, 10);
+    public static final Font fraktur = Resource.local().loadwait("ui/fraktur").layer(Resource.Font.class).font;
     public static final Font dfont = sans;
     public static final Foundry std;
     public final BufferedImage img;
