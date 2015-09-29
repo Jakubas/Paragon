b784f078662682de24821b6c58985bd787ce624c
romovs <romovs@gmail.com>
2015-09-29 20:34:32 +0300

    Draw quick slots background using pre-rendered tex for better performance

--- src/haven/QuickSlotsWdg.java
+++ src/haven/QuickSlotsWdg.java
@@ -1,42 +1,28 @@
 package haven;
 
-
-import java.awt.*;
-
 public class QuickSlotsWdg extends Widget implements DTarget {
-    private static final Color brdr = new Color(28, 35, 24);
-    private static final Color bg = new Color(40, 50, 34);
-    private static final Coord slotsz = new Coord(44, 44);
-    private static final Coord itemsz = new Coord(42, 42);
-    public static final Coord lc = Coord.z;
-    public static final Coord rc = new Coord(50, 0);
+    private static final Tex sbg = Resource.loadtex("gfx/hud/slots");
+    public static final Coord lc =  new Coord(6, 6);
+    public static final Coord rc = new Coord(56, 6);
+    private static final Coord ssz = new Coord(44, 44);
     private UI.Grab dragging;
     private Coord dc;
 
     public QuickSlotsWdg() {
         super(new Coord(44 + 44 + 6, 44));
-
     }
 
     @Override
     public void draw(GOut g) {
         Equipory e = getequipory();
         if (e != null) {
-            g.chcolor(brdr);
-            g.rect(lc, slotsz);
-            g.chcolor(bg);
-            g.frect(lc.add(1, 1), itemsz);
+            g.image(sbg, Coord.z);
             WItem left = e.quickslots[6];
             if (left != null)
-                drawitem(g.reclipl(lc.add(6, 6), g.sz), left);
-
-            g.chcolor(brdr);
-            g.rect(rc, slotsz);
-            g.chcolor(bg);
-            g.frect(rc.add(1, 1), itemsz);
+                drawitem(g.reclipl(lc, g.sz), left);
             WItem right = e.quickslots[7];
             if (right != null)
-                drawitem(g.reclipl(rc.add(6, 6), g.sz), right);
+                drawitem(g.reclipl(rc, g.sz), right);
         }
     }
 
@@ -44,16 +30,11 @@ public class QuickSlotsWdg extends Widget implements DTarget {
         GItem item = witem.item;
         GSprite spr = item.spr();
         if (spr != null) {
-            Coord sz = spr.sz();
             g.defstate();
             witem.drawmain(g, spr);
             g.defstate();
-            if (item.num >= 0)
-                g.atext(Integer.toString(item.num), sz, 1, 1);
-            else if (witem.itemnum.get() != null)
-                g.aimage(witem.itemnum.get(), sz, 1, 1);
         } else {
-            g.image(WItem.missing.layer(Resource.imgc).tex(), Coord.z, sz);
+            g.image(WItem.missing.layer(Resource.imgc).tex(), Coord.z, ssz);
         }
     }
 
