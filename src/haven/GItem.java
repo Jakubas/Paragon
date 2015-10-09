d53a8b1833143383e7c5c8ed1cef058505abfe14
romovs <romovs@gmail.com>
2015-10-09 17:13:56 +0300

    Fixed avg quality calculation

--- src/haven/GItem.java
+++ src/haven/GItem.java
@@ -88,8 +88,8 @@ public class GItem extends AWidget implements ItemInfo.SpriteOwner, GSprite.Owne
                 colormin = vitalityclr;
             }
 
-            double avg =  Math.cbrt(e * s * v);
-            double avgsv =  Math.sqrt(s * v);
+            double avg = Math.sqrt((double) (e * e + s * s + v * v) / 3.0);
+            double avgsv = Math.sqrt((double) (s * s + v * v) / 2.0);
             if (curio) {
                 double lpgain = Math.sqrt(Math.sqrt((double) (e * e + s * s + v * v) / 300.0));
                 lpgaintex = Text.renderstroked(longfmt.format(lpgain), Color.WHITE, Color.BLACK).tex();
