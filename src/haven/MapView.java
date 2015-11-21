6f9de8cdf455e428f3e9239c3ecca64f68a20085
romovs <romovs@gmail.com>
2015-11-21 18:48:49 +0200

    Tile center on itemact

--- src/haven/MapView.java
+++ src/haven/MapView.java
@@ -1492,6 +1492,10 @@ public class MapView extends PView implements DTarget, Console.Directory {
         delay(new Hittest(cc) {
             public void hit(Coord pc, Coord mc, ClickInfo inf) {
                 if (inf == null) {
+                    if (Config.tilecenter) {
+                        mc.x = mc.x / 11 * 11 + 5;
+                        mc.y = mc.y / 11 * 11 + 5;
+                    }
                     wdgmsg("itemact", pc, mc, ui.modflags());
                 } else {
                     if (inf.ol == null) {
