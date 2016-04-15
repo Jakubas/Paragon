8ca6bdf44d74b5c1ad3b85655b96341b8462b65e
romovs <romovs@gmail.com>
2016-04-15 18:19:50 +0300

    Removed debug printouts

--- src/haven/Skeleton.java
+++ src/haven/Skeleton.java
@@ -564,9 +564,6 @@ public class Skeleton {
     }
 
     public PoseMod mkposemod(ModOwner owner, Resource res, Message sdt) {
-
-        System.out.println("mkposemod: " + res.name);
-
         ModFactory f = res.getcode(ModFactory.class, false);
         if (f == null)
             f = ModFactory.def;
