3059496de238b99dadbcdd36b2bd499469cc55d2
romovs <romovs@gmail.com>
2016-06-18 07:42:21 +0300

    [locsan.sh] Drop duplicate records

--- etc/loc/locsan.sh
+++ etc/loc/locsan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 grep -rl $'\xEF\xBB\xBF' ../../l10n | while read l; do sed -i '1 s/^\xef\xbb\xbf//'  $l; done
 for i in ../../l10n/*.properties; do
-    sort ${i} -o ${i}
+    sort -u ${i} -o ${i}
 done
