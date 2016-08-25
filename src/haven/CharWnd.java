433d61890de79f11a5997212bbc1eb770dfd853d
romovs <romovs@gmail.com>
2016-08-26 00:03:56 +0300

    Reverted 3781828e
    It has been fixed server-side

--- src/haven/CharWnd.java
+++ src/haven/CharWnd.java
@@ -1395,8 +1395,6 @@ public class CharWnd extends Window {
                     } catch (Loading l) {
                         exp.sortkey = "\uffff";
                         loading = true;
-                    } catch (Exception e) { // FIXME: temp fix. missing tooltip in one of the new lores?
-                        exp.sortkey = "\uffff";
                     }
                 }
                 Arrays.sort(exps, comp);
