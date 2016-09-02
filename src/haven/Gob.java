Jakubas/Paragon

commit 1ca560bbe4bdc7824fb4262e5172db2bba2a9b1e

Fixed blinking last stage on grapes

src/haven/Gob.java
@@ -509,7 +509,7 @@ public class Gob implements Sprite.Owner, Skeleton.ModOwner, Rendered {
                             Overlay ol = findol(Sprite.GROWTH_STAGE_ID);
                             if (ol == null && (stage == cropstgmaxval || stage > 0 && stage < 5)) {
                                 addol(new Gob.Overlay(Sprite.GROWTH_STAGE_ID, new PlantStageSprite(stage, cropstgmaxval)));
-                            } else if (stage <= 0 || stage >= 5) {
+                            } else if (stage <= 0) {
                                 ols.remove(ol);
                             } else if (((PlantStageSprite)ol.spr).stg != stage) {
                                 ((PlantStageSprite)ol.spr).update(stage, cropstgmaxval);

src/haven/PlantStageSprite.java
@@ -33,7 +33,7 @@ public class PlantStageSprite extends Sprite {
     public void draw(GOut g) {
         mv.load(cam.load(camp.fin(Matrix4f.id))).mul1(wxf.load(loc.fin(Matrix4f.id)));
         Coord3f s = proj.toscreen(mv.mul4(Coord3f.o), wndsz);
-        g.image(tex, new Coord((int) s.x - 5, (int) s.y - 20));
+        g.image(tex, new Coord((int) s.x - 7, (int) s.y - 10));
     }
 
     public boolean setup(RenderList rl) {

commit 0624c6a1ad0d506576fce84a51e8bd44d5d11faa

Fixed regression introduced in 1ca560bb

src/haven/Gob.java
@@ -497,24 +497,21 @@ public class Gob implements Sprite.Owner, Skeleton.ModOwner, Rendered {
                 if (res != null && res.name.startsWith("gfx/terobjs/plants") && !res.name.endsWith("trellis")) {
                     GAttrib rd = getattr(ResDrawable.class);
                     if (rd != null) {
-                        try {
-                            int stage = ((ResDrawable) rd).sdt.peekrbuf(0);
-                            if (cropstgmaxval == 0) {
-                                for (FastMesh.MeshRes layer : res.layers(FastMesh.MeshRes.class)) {
-                                    int stg = layer.id / 10;
-                                    if (stg > cropstgmaxval)
-                                        cropstgmaxval = stg;
-                                }
+                        int stage = ((ResDrawable) rd).sdt.peekrbuf(0);
+                        if (cropstgmaxval == 0) {
+                            for (FastMesh.MeshRes layer : res.layers(FastMesh.MeshRes.class)) {
+                                int stg = layer.id / 10;
+                                if (stg > cropstgmaxval)
+                                    cropstgmaxval = stg;
                             }
-                            Overlay ol = findol(Sprite.GROWTH_STAGE_ID);
-                            if (ol == null && (stage == cropstgmaxval || stage > 0 && stage < 5)) {
-                                addol(new Gob.Overlay(Sprite.GROWTH_STAGE_ID, new PlantStageSprite(stage, cropstgmaxval)));
-                            } else if (stage <= 0) {
-                                ols.remove(ol);
-                            } else if (((PlantStageSprite)ol.spr).stg != stage) {
-                                ((PlantStageSprite)ol.spr).update(stage, cropstgmaxval);
-                            }
-                        } catch (ArrayIndexOutOfBoundsException e) { // ignored
+                        }
+                        Overlay ol = findol(Sprite.GROWTH_STAGE_ID);
+                        if (ol == null && (stage == cropstgmaxval || stage > 0 && stage < 6)) {
+                            addol(new Gob.Overlay(Sprite.GROWTH_STAGE_ID, new PlantStageSprite(stage, cropstgmaxval)));
+                        } else if (stage <= 0 || stage >= 6) {
+                            ols.remove(ol);
+                        } else if (((PlantStageSprite)ol.spr).stg != stage) {
+                            ((PlantStageSprite)ol.spr).update(stage, cropstgmaxval);
                         }
                     }
                 }
@@ -522,17 +519,14 @@ public class Gob implements Sprite.Owner, Skeleton.ModOwner, Rendered {
                 if (res != null && (res.name.startsWith("gfx/terobjs/trees") || res.name.startsWith("gfx/terobjs/bushes"))) {
                     ResDrawable rd = getattr(ResDrawable.class);
                     if (rd != null && !rd.sdt.eom()) {
-                        try {
-                            final int stage = rd.sdt.peekrbuf(0);
-                            if (stage < 100) {
-                                Overlay ol = findol(Sprite.GROWTH_STAGE_ID);
-                                if (ol == null) {
-                                    addol(new Gob.Overlay(Sprite.GROWTH_STAGE_ID, new TreeStageSprite(stage)));
-                                } else if (((TreeStageSprite)ol.spr).val != stage) {
-                                    ((TreeStageSprite)ol.spr).update(stage);
-                                }
+                        final int stage = rd.sdt.peekrbuf(0);
+                        if (stage < 100) {
+                            Overlay ol = findol(Sprite.GROWTH_STAGE_ID);
+                            if (ol == null) {
+                                addol(new Gob.Overlay(Sprite.GROWTH_STAGE_ID, new TreeStageSprite(stage)));
+                            } else if (((TreeStageSprite)ol.spr).val != stage) {
+                                ((TreeStageSprite)ol.spr).update(stage);
                             }
-                        } catch (ArrayIndexOutOfBoundsException e) { // ignored
                         }
                     }
                 }

src/haven/PlantStageSprite.java
@@ -13,7 +13,8 @@ public class PlantStageSprite extends Sprite {
             Text.renderstroked("2", stagecolor, Color.BLACK, fndr).tex(),
             Text.renderstroked("3", stagecolor, Color.BLACK, fndr).tex(),
             Text.renderstroked("4", stagecolor, Color.BLACK, fndr).tex(),
-            Text.renderstroked("5", stagecolor, Color.BLACK, fndr).tex()
+            Text.renderstroked("5", stagecolor, Color.BLACK, fndr).tex(),
+            Text.renderstroked("6", stagecolor, Color.BLACK, fndr).tex()
     };
     public int stg;
     private Tex tex;
@@ -33,7 +34,7 @@ public class PlantStageSprite extends Sprite {
     public void draw(GOut g) {
         mv.load(cam.load(camp.fin(Matrix4f.id))).mul1(wxf.load(loc.fin(Matrix4f.id)));
         Coord3f s = proj.toscreen(mv.mul4(Coord3f.o), wndsz);
-        g.image(tex, new Coord((int) s.x - 7, (int) s.y - 10));
+        g.image(tex, new Coord((int) s.x - tex.sz().x/2, (int) s.y - 10));
     }
 
     public boolean setup(RenderList rl) {